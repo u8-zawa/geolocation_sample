50a90201f5991d42cf405a61fa7ba7040aa1a60f
yu-8zawa <A.zawawa@icloud.com>
2022-02-09 20:16:43 +0900

    :sparkles:Add geolocator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:geolocator/geolocator.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,11 +10,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+  String _location = 'no data';
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _requestPermission();
+  }
+
+  Future<void> _requestPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.',
+      );
+    }
+  }
+
+  Future<void> getLocation() async {
+    // 現在の位置を返す
+    final position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    // 北緯がプラス。南緯がマイナス
+    debugPrint('緯度: ${position.latitude}');
+    // 東経がプラス、西経がマイナス
+    debugPrint('経度: ${position.longitude}');
+    // 高度
+    debugPrint('高度: ${position.altitude}');
+    // 距離をメートルで返す
+    final distanceInMeters =
+        Geolocator.distanceBetween(35.68, 139.76, -23.61, -46.40);
+    debugPrint(distanceInMeters.toString());
+    // 方位を返す
+    final bearing = Geolocator.bearingBetween(35.68, 139.76, -23.61, -46.40);
+    debugPrint(bearing.toString());
     setState(() {
-      _counter++;
+      _location = position.toString();
     });
   }
 
@@ -26,20 +74,13 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text(_location),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: getLocation,
+        child: const Icon(Icons.location_on),
       ),
     );
   }
